ca16caf6dd7672da8601b7149abc742d6a0f88d6
Marius Lundgård <studio@mariuslundgard.com>
2021-08-24 21:47:17 +0200

    test(desk-tool): update e2e test

--- cypress/integration/default-layout/navbar.test.js
+++ cypress/integration/default-layout/navbar.test.js
@@ -10,8 +10,10 @@ describe('@sanity/default-layout: Navbar', () => {
       '500401'
     )
 
-    cy.get(
-      '[data-testid="desk-tool-list-pane"] [data-test="components-default-pane-header"]'
-    ).should('have.css', 'z-index', '101')
+    cy.get('[data-testid="desk-tool-list-pane"] [data-ui="PaneHeader"]').should(
+      'have.css',
+      'z-index',
+      '101'
+    )
   })
 })
