cf44cc7eaa2249ff0f28f1ad23cfe30b9781a00f
hermanwikner <wiknerherman@hotmail.com>
2021-08-31 09:22:16 +0200

    refactor(default-layout): migrate `DefaultLayout` to `@sanity/ui` and refactor to functional component

---
+++ packages/@sanity/default-layout/src/defaultLayout/DefaultLayout.tsx
@@ -0,0 +1,162 @@
+// @todo: remove the following line when part imports has been removed from this file
+///<reference types="@sanity/types/parts" />
+
+import React, {useState, useEffect, useCallback} from 'react'
+import {RouteScope, withRouterHOC} from 'part:@sanity/base/router'
+import absolutes from 'all:part:@sanity/base/absolutes'
+import userStore from 'part:@sanity/base/user'
+import {LegacyLayerProvider} from '@sanity/base/components'
+import {Box} from '@sanity/ui'
+import Sidecar from '../addons/Sidecar'
+import RenderTool from '../main/RenderTool'
+import {CreateDocumentDialog} from '../createDocumentDialog'
+import {SchemaErrorReporter} from '../schemaErrors/SchemaErrorReporter'
+import {SideMenu} from '../sideMenu'
+import getNewDocumentModalActions from '../util/getNewDocumentModalActions'
+import {Router, Tool, User} from '../types'
+import {Navbar} from '../navbar'
+import {Root, MainArea, ToolContainer, SidecarContainer} from './styles'
+import {LoadingScreen} from './LoadingScreen'
+
+interface OuterProps {
+  tools: Tool[]
+}
+
+interface Props {
+  router: Router
+  tools: Tool[]
+}
+
+export const DefaultLayout = withRouterHOC((props: Props) => {
+  const {tools, router} = props
+  const [createMenuIsOpen, setCreateMenuIsOpen] = useState<boolean>(false)
+  const [menuIsOpen, setMenuIsOpen] = useState<boolean>(false)
+  const [showLoadingScreen, setShowLoadingScreen] = useState<boolean>(true)
+  const [loaded, setLoaded] = useState<boolean>(false)
+  const [user, setUser] = useState<User>()
+  const [searchIsOpen, setSearchIsOpen] = useState<boolean>(false)
+  const [loadingScreenElement, setLoadingScreenElement] = useState<HTMLDivElement | null>(null)
+  const [portalElement, setPortalElement] = useState<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    userStore.me.subscribe((u: User) => setUser(u))
+  }, [])
+
+  useEffect(() => {
+    if (!loaded) {
+      setLoaded(true)
+    }
+  }, [loaded])
+
+  const handleAnimationEnd = useCallback(() => {
+    setShowLoadingScreen(false)
+  }, [])
+
+  useEffect(() => {
+    if (loadingScreenElement) {
+      loadingScreenElement.addEventListener('animationend', handleAnimationEnd, false)
+    }
+    return () => {
+      if (loadingScreenElement) {
+        loadingScreenElement.removeEventListener('animationend', handleAnimationEnd, false)
+      }
+    }
+  }, [handleAnimationEnd, loadingScreenElement])
+
+  const handleCreateButtonClick = useCallback(() => {
+    setCreateMenuIsOpen((prev) => !prev)
+  }, [])
+
+  const handleActionModalClose = useCallback(() => {
+    setCreateMenuIsOpen(false)
+  }, [])
+
+  const handleToggleMenu = useCallback(() => {
+    setMenuIsOpen((prev) => !prev)
+  }, [])
+
+  const handleSwitchTool = useCallback(() => {
+    setMenuIsOpen(false)
+  }, [])
+
+  const handleLogout = useCallback(() => {
+    userStore.actions.logout()
+  }, [])
+
+  const handleSearchOpen = useCallback((open) => {
+    setSearchIsOpen(open)
+  }, [])
+
+  const renderContent = () => {
+    const isOverlayVisible = menuIsOpen || searchIsOpen
+    const tool = router?.state?.tool || ''
+    const documentTypes = getNewDocumentModalActions().map((action) => action.schemaType)
+
+    return (
+      <Root $isOverlayVisible={isOverlayVisible}>
+        {showLoadingScreen && (
+          <LoadingScreen
+            loaded={loaded || document.visibilityState == 'hidden'}
+            ref={setLoadingScreenElement}
+          />
+        )}
+
+        <LegacyLayerProvider zOffset="navbar">
+          <Navbar
+            tools={tools}
+            createMenuIsOpen={createMenuIsOpen}
+            onCreateButtonClick={handleCreateButtonClick}
+            onToggleMenu={handleToggleMenu}
+            router={router}
+            documentTypes={documentTypes}
+            onUserLogout={handleLogout}
+            searchIsOpen={handleSearchOpen}
+            searchPortalElement={portalElement}
+          />
+        </LegacyLayerProvider>
+
+        {user && (
+          <SideMenu
+            activeToolName={tool}
+            isOpen={menuIsOpen}
+            onClose={handleToggleMenu}
+            onSignOut={handleLogout}
+            onSwitchTool={handleSwitchTool}
+            router={router}
+            tools={tools}
+            user={user}
+          />
+        )}
+
+        <MainArea $isOverlayVisible={isOverlayVisible}>
+          <ToolContainer hidden={searchIsOpen}>
+            <RouteScope scope={tool}>
+              <RenderTool tool={tool} />
+            </RouteScope>
+          </ToolContainer>
+
+          <SidecarContainer hidden={searchIsOpen}>
+            <Sidecar />
+          </SidecarContainer>
+
+          <Box flex={1} ref={setPortalElement} style={{position: 'absolute', inset: 0}} />
+        </MainArea>
+
+        {createMenuIsOpen && (
+          <LegacyLayerProvider zOffset="navbar">
+            <CreateDocumentDialog
+              onClose={handleActionModalClose}
+              actions={getNewDocumentModalActions()}
+            />
+          </LegacyLayerProvider>
+        )}
+
+        {absolutes.map((Abs, i) => (
+          <Abs key={String(i)} />
+        ))}
+      </Root>
+    )
+  }
+
+  return <SchemaErrorReporter>{renderContent}</SchemaErrorReporter>
+}) as React.ComponentType<OuterProps>
---
+++ packages/@sanity/default-layout/src/defaultLayout/styles.ts
@@ -0,0 +1,62 @@
+import styled, {css} from 'styled-components'
+import {Flex} from '@sanity/ui'
+
+export const Root = styled(Flex)<{$isOverlayVisible: boolean}>`
+  box-sizing: border-box;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  ${({$isOverlayVisible}) =>
+    $isOverlayVisible &&
+    css`
+      overflow: hidden;
+      height: 100%;
+    `}
+
+  @media (min-width: ${({theme}) => theme.sanity.media[1]}px) {
+    overflow: hidden;
+    width: 100%;
+    height: 100%;
+  }
+`
+
+export const MainArea = styled(Flex)<{$isOverlayVisible: boolean}>`
+  flex: 1;
+  min-height: 0;
+  position: relative;
+
+  ${({$isOverlayVisible}) =>
+    $isOverlayVisible &&
+    css`
+      overflow: hidden;
+      height: 100%;
+    `}
+`
+
+export const ToolContainer = styled.div`
+  position: relative;
+  flex: 1;
+  min-width: 0;
+  height: 100%;
+
+  @media (min-width: ${({theme}) => theme.sanity.media[1]}px) {
+    overflow: auto;
+  }
+`
+
+export const SidecarContainer = styled.div`
+  position: relative;
+
+  @media (max-width: ${({theme}) => theme.sanity.media[1]}px) {
+    display: none;
+  }
+
+  @media (min-width: ${({theme}) => theme.sanity.media[1]}px) {
+    width: 420px;
+
+    &:empty {
+      display: none;
+    }
+  }
+`
