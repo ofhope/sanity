a058a43e5ded063dafa4993ffcd15f8eff4511db
Marius Lundgård <studio@mariuslundgard.com>
2021-09-02 16:34:39 +0200

    refactor(desk-tool): improve types in DocumentTitle

--- packages/@sanity/desk-tool/src/components/DocTitle.tsx
+++ packages/@sanity/desk-tool/src/components/DocTitle.tsx
@@ -7,14 +7,14 @@ import schema from 'part:@sanity/base/schema'
 import {PreviewFields} from 'part:@sanity/base/preview'
 
 export interface DocTitleProps {
-  document: SanityDocument
+  document: Partial<SanityDocument>
 }
 
 const renderTitle = ({title}: {title?: string}) => <span>{title}</span>
 
 export function DocTitle(props: DocTitleProps) {
   const {document} = props
-  const type = schema.get(document._type)
+  const type = schema.get(document._type!)
 
   return (
     <PreviewFields document={document} type={type} fields={['title']}>
