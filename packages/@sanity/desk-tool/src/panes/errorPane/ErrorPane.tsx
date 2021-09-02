97503d4c68cbe755da6a499d458f178732f65041
Marius Lundgård <studio@mariuslundgard.com>
2021-09-02 19:13:08 +0200

    refactor(desk-tool): use new pane components in ErrorPane

--- packages/@sanity/desk-tool/src/panes/errorPane/ErrorPane.tsx
+++ packages/@sanity/desk-tool/src/panes/errorPane/ErrorPane.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import DefaultPane from 'part:@sanity/components/panes/default'
-import styles from './ErrorPane.css'
+import {Box, CardTone} from '@sanity/ui'
+import {Pane, PaneContent, PaneHeader} from '../../components/pane'
 
-export function ErrorPane(props: {children?: React.ReactNode}) {
+export function ErrorPane(props: {
+  children?: React.ReactNode
+  flex?: number
+  minWidth?: number
+  title?: React.ReactNode
+  tone?: CardTone
+}) {
+  const {children, flex, minWidth, title = 'Error', tone = 'critical'} = props
+
   return (
-    <DefaultPane color="danger" title="Error" isSelected={false} isCollapsed={false}>
-      <div className={styles.root}>{props.children}</div>
-    </DefaultPane>
+    <Pane flex={flex} minWidth={minWidth} tone={tone}>
+      <PaneHeader title={title} />
+      <PaneContent overflow="auto">
+        <Box paddingX={4} paddingY={5}>
+          {children}
+        </Box>
+      </PaneContent>
+    </Pane>
   )
 }
