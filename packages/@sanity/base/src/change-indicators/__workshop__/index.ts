a7cd104f0828141ef8b536d62debefcd1b51e423
Marius Lundgård <studio@mariuslundgard.com>
2021-09-02 14:53:02 +0200

    test(base): improve workshop for change indicators

--- packages/@sanity/base/src/change-indicators/__workshop__/index.ts
+++ packages/@sanity/base/src/change-indicators/__workshop__/index.ts
@@ -3,8 +3,13 @@ import {lazy} from 'react'
 
 export default defineScope('base/change-indicators', 'Change indicators', [
   {
-    name: 'changebar',
+    name: 'change-bar',
     title: 'ChangeBar',
     component: lazy(() => import('./example')),
+  },
+  {
+    name: 'change-connector',
+    title: 'ChangeConnector',
+    component: lazy(() => import('./changeConnector')),
   },
 ])
