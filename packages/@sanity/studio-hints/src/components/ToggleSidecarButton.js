f267a5e2861118cce9e36b8bcced30a811584208
hermanwikner <wiknerherman@hotmail.com>
2021-09-05 16:11:44 +0200

    fix(studio-hints): design fixes on `ToggleSidecarButton`

--- packages/@sanity/studio-hints/src/components/ToggleSidecarButton.js
+++ packages/@sanity/studio-hints/src/components/ToggleSidecarButton.js
@@ -5,19 +5,8 @@ import {
   isSidecarOpenSetting,
   toggleSidecarOpenState,
 } from 'part:@sanity/default-layout/sidecar-datastore'
-import {Button as UIButton, studioTheme, ThemeProvider} from '@sanity/ui'
+import {Button} from '@sanity/ui'
 import {HelpCircleIcon} from '@sanity/icons'
-import styled from 'styled-components'
-
-const Button = styled(UIButton)`
-  width: 33px;
-  height: 33px;
-
-  &:not(:hover):not(:focus):not([data-selected]) {
-    background: transparent;
-    box-shadow: unset;
-  }
-`
 
 export default class ToggleSidecarButton extends React.PureComponent {
   state = {
@@ -40,18 +29,14 @@ export default class ToggleSidecarButton extends React.PureComponent {
     const {isOpen} = this.state
 
     return (
-      <ThemeProvider theme={studioTheme} scheme="dark">
-        <Button
-          padding={0}
-          aria-label="Toggle sidecar"
-          aria-pressed={isOpen}
-          icon={HelpCircleIcon}
-          onClick={toggleSidecarOpenState}
-          selected={isOpen}
-          tone="primary"
-          mode="bleed"
-        />
-      </ThemeProvider>
+      <Button
+        aria-label="Toggle sidecar"
+        aria-pressed={isOpen}
+        icon={HelpCircleIcon}
+        onClick={toggleSidecarOpenState}
+        selected={isOpen}
+        mode="bleed"
+      />
     )
   }
 }
